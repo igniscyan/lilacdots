4a527c4d9704510b552dd9a3c9cd95cc5bdd405b
Brandon Clark <bwclark97@gmail.com>
2023-02-12 18:55:38 -0500

    Pushing dots

---
+++ fishWSL/conf.d/tide.fish
@@ -0,0 +1,40 @@
+#GIT COLOR OPTIONS
+set -g tide_git_bg_color 458588
+set -g tide_git_bg_color_unstable d79921 
+set -g tide_git_bg_color_urgent cc241d
+set -g tide_git_branch_color 000000
+set -g tide_git_color_branch 000000
+set -g tide_git_color_conflicted 000000
+set -g tide_git_color_dirty 000000
+set -g tide_git_color_operation 000000
+set -g tide_git_color_staged 000000
+set -g tide_git_color_stash 000000
+set -g tide_git_color_untracked 000000
+set -g tide_git_color_upstream 000000
+set -g tide_git_conflicted_color 000000
+set -g tide_git_dirty_color 000000
+set -g tide_git_icon 
+set -g tide_git_operation_color 000000
+set -g tide_git_staged_color 000000
+set -g tide_git_stash_color 000000
+set -g tide_git_untracked_color 000000
+set -g tide_git_upstream_color 000000
+#PWD OPTIONS
+set -g tide_pwd_bg_color 3c3836
+set -g tide_pwd_color_dirs ebdbb2
+set -g tide_pwd_color_anchors fbf1c7
+set -g tide_pwd_home_icon 
+set -g tide_pwd_icon_unwritable 
+set -g tide_pwd_icon 
+#VERSIONING STATUS COLORS
+set -g tide_node_bg_color 98971a
+set -g tide_node_color FFFFFF
+#cmd_duration COLORS
+set -g tide_cmd_duration_bg_color 3c3836
+set -g tide_cmd_duration_color ebdbb2
+#TIME COLORS
+set -g tide_time_bg_color 3c3836
+set -g tide_time_color ebdbb2
+#STATUS COLORS
+set -g tide_status_bg_color_failure cc241d
+set -g tide_status_color_failure ebdbb2
